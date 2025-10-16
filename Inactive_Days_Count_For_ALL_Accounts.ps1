08aa501c4d3b4f3f3d11c4ae8b0cd41942c8049a
Göksel <ybqdqxstsb@privaterelay.appleid.com>
2025-10-16 21:35:03 +0300

    Inactive Days For ALL Accounts
    SharedMailbox etc. All kind of accounts with inactive days.

---
+++ Inactive_Days_Count_For_ALL_Accounts.ps1
@@ -0,0 +1,37 @@
+﻿$day = 90
+$date = (Get-Date).AddDays(-$day)
+
+# Find inactive users
+$inactiveUsers = Get-ADUser -Filter {LastLogonDate -lt $date -and Enabled -eq $true} -Properties LastLogonDate, DistinguishedName, emailaddress |
+    Select-Object Name, SamAccountName, LastLogonDate, DistinguishedName, emailaddress |
+    Sort-Object LastLogonDate -Descending
+
+# Determine the number of days they have been inactive
+function DaysInactive ([datetime]$LastLogonDate) {
+    return ((Get-Date) - $LastLogonDate).Days
+}
+
+# Function to get OU information
+function Get-OU([string]$DistinguishedName) {
+    $ouParts = $DistinguishedName -split ',' | Where-Object { $_ -like "OU=*" }
+    $ou = $ouParts -join ',' # Create the OU information
+    return $ou
+}
+
+# Process user data and write to CSV file
+$results = foreach ($user in $inactiveUsers) {
+    $daysInactive = DaysInactive -LastLogonDate $user.LastLogonDate
+    $ou = Get-OU -DistinguishedName $user.DistinguishedName
+
+    [PSCustomObject]@{
+        Name               = $user.Name
+        SamAccountName     = $user.SamAccountName
+        LastLogonDate      = $user.LastLogonDate
+        DaysInactive       = $daysInactive
+        OrganizationalUnit = $ou
+        Mail               = $user.emailaddress
+    }
+}
+
+# Export results to CSV file
+$results | Export-Csv -Path "C:\Temp\inactiveusers.csv" -NoTypeInformation -Encoding UTF8
